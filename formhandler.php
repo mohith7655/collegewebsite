dcf08c55097c98dd11d2ff2093c5a6d9475b2006
mohith7655 <s.mohithkumar@gmail.com>
2021-06-19 00:33:51 +0530

    finalreport

---
+++ formhandler.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 's.mohithkumar@gmail.com';
+$email_subject = 'New form submission';
+$email_body = "user name: $name.\n".
+                     "user name: $visitor_email.\n".
+                  "subject: $subject.\n"
+                  "user message: $message .\n";
+
+$to = 's.mohithkumar@outlook.com';                  
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+mail($to,$email_subjcet,$email_body,$headers)
+header("Location: contact.html");
+?>
